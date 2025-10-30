4933b76c0ae0e66320defb9b9bbaecda3399e9ad
Natalia <natarose727@gmail.com>
2025-10-30 12:33:17 +0100

    feat: completed exercise 1.5.3 - find method

--- 1.5-array-transformations/Exs1.5.js
+++ 1.5-array-transformations/Exs1.5.js
@@ -1,7 +1,7 @@
 console.log('Level 1')
 console.log('1.5.1: Map')
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 console.log('Array original', numeros)
 
 const alCuadrado = numeros.map(num => num * num )
@@ -13,3 +13,8 @@ console.log('1.5.2: Filter')
 const pares = numeros.filter(num => num % 2 === 0)
 
 console.log('Array de pares (filter)', pares)
+
+console.log('1.5.3: Find')
+
+const mayorADiez = numeros.find(num => num > 10)
+console.log('Primer número mayor a 10 (find):', mayorADiez)
