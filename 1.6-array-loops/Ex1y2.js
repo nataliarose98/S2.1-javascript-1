88e578fe1ea3b2a7d726bdbc606418e8359e05c6
Natalia <natarose727@gmail.com>
2025-11-04 12:20:04 +0100

    Feat: completed exercise 1.6.1- forEach

---
+++ 1.6-array-loops/Ex1y2.js
@@ -0,0 +1,10 @@
+console.log('Level 1')
+console.log('ForEach')
+
+arrayNombres = ['Anna', 'Bernat', 'Clara', 'Luis', 'Juan']
+console.log('Array de Nombres', arrayNombres)
+
+console.log('Imprimiendo con forEach()')
+arrayNombres.forEach(nombre => {
+    console.log(nombre)
+})
